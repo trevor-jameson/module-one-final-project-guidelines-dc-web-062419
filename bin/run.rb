0b42f23daa27cf4e922dec748b4b7f36a8f08f7c
Harum Helmy <harumendhah@gmail.com>
2019-07-09 17:38:42 -0400

    changing drivers, made some things work, some things worse

--- bin/run.rb
+++ bin/run.rb
@@ -1,31 +1,43 @@
 require_relative '../config/environment'
 require 'pry'
 
-puts "WELCOME"
+puts "Welcome, young adventurer."
 puts "If you accept this quest..."
-
 # Choose a character
 puts Character.all.map { |character| character.name }
 puts "Enter the name of the character you'd like to use:"
 chosen_character = gets.chomp
-if chosen_character == "Harum the Witch"
-    puts "Welcome, Harum the Witch!"
-    chosen_character = Character.find(1)
-elsif chosen_character == "Ashley the Wizard"
-    puts "Welcome, Ashley the Wizard!"
-    chosen_character = Character.find(2)
-else
-    puts "Choose a character."
-    chosen_character = gets.chomp
-end
+
+    if chosen_character == "Harum the Witch"
+        puts "Welcome, Harum the Witch!"
+        chosen_character = Character.find(1)
+    elsif chosen_character == "Ashley the Wizard"
+        puts "Welcome, Ashley the Wizard!"
+        chosen_character = Character.find(2)
+    else
+        puts "Choose a character."
+        chosen_character = gets.chomp
+    end
 
 # # Enter a room
-puts "You've entered the GRAND ENTRANCE."
-# puts entry_room.description
+puts "You've entered the Grand Entrance."
+
+# enter this room - 
+
+# write method to update the character's room_id
+# chosen_character.room_id = room.id
+
+
 puts "You see a dagger and a scroll - which will you choose?"
+
+# write method to update the items' room_ids
+
 item_id = Item.all.collect { |item| item.id }
+
 # binding.pry
+
 puts "Enter 1 for dagger or 4 for scroll"
+
 character_item = gets.chomp.to_i
 
 # depending on what the user chooses, we need to set that item's character_id to
