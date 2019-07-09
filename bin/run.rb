228973c4b965b6968b3e54022eb8c114afa2af36
AsheToll <github email address>
2019-07-09 15:27:23 -0400

    Starting to write our run.rb.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,16 @@
 require_relative '../config/environment'
-
-
-
-puts "HELLO WORLD"
+require 'pry'
+puts "WELCOME"
+puts "If you accept this quest..."
+# Choose a character
+puts Character.all.map {|character| character.name}
+puts "Enter the name of the character you'd like to use:"
+user_input = gets.chomp
+if user_input == "Harum the Witch"
+    puts "Welcome, Harum the Witch!"
+elsif user_input == "Ashley the Wizard"
+    puts "Welcome, Ashley the Wizard!"
+else
+    puts "Choose a character."
+    user_input = gets.chomp
+end
