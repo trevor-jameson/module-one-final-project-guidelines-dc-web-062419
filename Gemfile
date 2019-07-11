7cf23c4ca99d6f8d64ae053d56c777051176549e
AsheToll <github email address>
2019-07-11 16:56:54 -0400

    Started working on room navigation and logic.

--- Gemfile
+++ Gemfile
@@ -6,4 +6,4 @@ gem "sqlite3"
 gem "pry"
 gem "require_all"
 gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
-
+gem 'tty-prompt'
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,7 @@
 require_relative '../config/environment'
 require 'pry'
+require 'tty-prompt'
+prompt = TTY::Prompt.new
 
 ### WELCOME USERS TO CLI ###
 def line_break
@@ -28,22 +30,56 @@ Character.welcome_to_entrance
 
 ### LIST ITEMS IN GRAND ENTRANCE ### 
 room = Room.find_by(name: "Grand Entrance")
-puts room.list_items
 
-Item.get_character_item
-Item.selected_item
+room.navigate_from_the_entrance
+# ### ROOM NAVIGATION ###
+# # def navigate_from_entrance
+#     puts "You look around the room and you see a chest to your left, an enchantment table in front, and a bookcase to your right."
+#     puts "Which do you explore first?"
+#     puts "Enter 'left', 'forward', or 'right'."
+#     direction = gets.chomp
+# # end
 
-# puts "You've entered the #{room}. You see #{items}."
-
-# Item.get_character_item
-# Item.return_selection
-# character_item = gets.chomp.to_i
-
-# # depending on what the user chooses, we need to set that item's character_id to
-# # the user's character
-# Item.all.each do |item|
-#     if item.id == character_item
-#         item.character_id = chosen_character.id
-#         item.save
-#     end
+# def locked_chest
+#     puts "You walk up to a chest - it's locked."
+#     puts "Turn around and go left to the enchantment table, or forward to the bookcase?"
+#     # puts "Enter 'left' or 'forward'."
+#     direction = gets.chomp
 # end
+
+# def enchantment_table
+#     puts "You approach the enchantment table (colorful description)."
+#     puts "There are potions lining the edge, and a glowing crystal ball."
+#     # pick up potion, crystal ball, or both?
+# end
+
+# def bookcase
+#     puts "You skim the bookcase, and see a bunch of old, dust-covered scrolls."
+#     # if they pick up the scroll, they see dagger underneath and have option of picking it up
+#     puts "Hiding underneath the scroll you grab is a dagger."
+# end
+
+# # # if from locked_chest
+# # if direction == 'left'
+# #     puts "You approach the enchantment table (colorful description)."
+# #     puts "There are potions lining the edge, and a glowing crystal ball."
+# #     # pick up potion, crystal ball, or both?
+# # end
+
+# # from the entrance
+# if direction == 'left' #(locked)
+#     locked_chest
+# elsif direction == 'forward'
+#     enchantment_table
+# elsif direction == 'right'
+#     bookcase
+# else
+#     puts "You look around the room and you see a chest to your left, an enchantment table in front, and a bookcase to your right."
+#     puts "Which do you explore first?"
+#     puts "Enter 'left', 'forward', or 'right'."
+# end
+# # from the chest
+# ## def locked_chest
+# ## def unlocked_chest
+# # from the enchantment table
+# # from the bookcase
